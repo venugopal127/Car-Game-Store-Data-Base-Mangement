1cc1fa0751b7072ae2f1584db78bdcef3df04011
venugopal127 <147709188+venugopal127@users.noreply.github.com>
2024-07-18 16:18:26 +0530

    Add files via upload

---
+++ src/session.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author venug
+ */
+public class session {
+    private static int userId;
+
+    public static void setUserId(int userId) {
+        session.userId = userId;
+    }
+
+    public static int getUserId() {
+        return userId;
+    }
+}
+
